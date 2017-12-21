8a53236570d498ddb89065b5caed072cc763c9d0
S L <san98der@hotmail.com>
2017-12-21 22:07:47 +0100

    fixed views

--- todo/views.py
+++ todo/views.py
@@ -69,3 +69,16 @@ def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
     return redirect('post_detail', pk=comment.post.pk)
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'todo/add_comment_to_post.html', {'form': form})
